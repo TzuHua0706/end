cd6c59da091f909ecabc99340033ac7e646b9397
unknown <>
2017-01-02 21:24:58 +0800

    commit first

---
+++ js/Pixar.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(".img_down").click(function(){
+    $("footer").removeClass("clack");
+    /*if( $("nav").hasClass("click") ){
+      $("nav").removeClass("click");
+      $("nav ul").slideUp();
+    }else{
+      $("nav").addClass("click");
+      $("nav ul").slideDown();
+      return false;
+    }*/
+  });
+});
