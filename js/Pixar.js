ebebe6fa63788b77b9c1ad516e3a32d120fc1590
unknown <>
2017-01-04 09:38:48 +0800

    first

--- js/Pixar.js
+++ js/Pixar.js
@@ -1,13 +1,36 @@
 $(document).ready(function(){
   $(".img_down").click(function(){
-    $("footer").removeClass("clack");
-    /*if( $("nav").hasClass("click") ){
-      $("nav").removeClass("click");
-      $("nav ul").slideUp();
-    }else{
-      $("nav").addClass("click");
-      $("nav ul").slideDown();
-      return false;
-    }*/
+    if($('.history_5').offset().top<$('.img_down').offset().top){
+      $('html,body').animate({scrollTop: $('.history_5').offset().top},500);
+    }
+    else {
+      if($('.history_4').offset().top<$('.img_down').offset().top){
+        $('html,body').animate({scrollTop: $('.history_4').offset().top},500);
+      }
+      else {
+        if($('.history_3').offset().top<$('.img_down').offset().top){
+          $('html,body').animate({scrollTop: $('.history_3').offset().top},500);
+        }
+        else{
+          if($('.history_2').offset().top<$('.img_down').offset().top){
+            $('html,body').animate({scrollTop: $('.history_2').offset().top},500);
+          }
+          else{
+            if($('.history_title').offset().top<$('.img_down').offset().top){
+              $('html,body').animate({scrollTop: $('.history_title').offset().top},500);
+            }
+            else {
+              if($('.p_where').offset().top<$('.img_down').offset().top){
+              $('html,body').animate({scrollTop: $('.p_where').offset().top},500);
+              }
+              else{
+                $('html,body').animate({scrollTop: $('.p_time').offset().top},500);
+              }
+            }
+          }
+        }
+      }
+    }
   });
 });
+  /*$(selector).animate(styles,speed,easing,callback)*/
