738a60a1efdf07e536c467c7aa85d70d07e90ef7
unknown <>
2017-01-04 10:43:27 +0800

    first

--- js/Pixar.js
+++ js/Pixar.js
@@ -1,30 +1,58 @@
 $(document).ready(function(){
+if ($('.footer').offset().top<$('.img_down').offset().top) {
+  $(".img_down").addClass("display");
+}
   $(".img_down").click(function(){
-    if($('.history_5').offset().top<$('.img_down').offset().top){
-      $('html,body').animate({scrollTop: $('.history_5').offset().top},500);
+    if($('.history_9').offset().top<$('.img_down').offset().top){
+      $('html,body').animate({scrollTop: $('.history_9').offset().top},500);
     }
     else {
-      if($('.history_4').offset().top<$('.img_down').offset().top){
-        $('html,body').animate({scrollTop: $('.history_4').offset().top},500);
+      if($('.history_8').offset().top<$('.img_down').offset().top){
+        $('html,body').animate({scrollTop: $('.history_8').offset().top},500);
       }
       else {
-        if($('.history_3').offset().top<$('.img_down').offset().top){
-          $('html,body').animate({scrollTop: $('.history_3').offset().top},500);
+        if($('.history_7').offset().top<$('.img_down').offset().top){
+          $('html,body').animate({scrollTop: $('.history_7').offset().top},500);
         }
-        else{
-          if($('.history_2').offset().top<$('.img_down').offset().top){
-            $('html,body').animate({scrollTop: $('.history_2').offset().top},500);
+        else {
+          if($('.history_6').offset().top<$('.img_down').offset().top){
+            $('html,body').animate({scrollTop: $('.history_6').offset().top},500);
           }
-          else{
-            if($('.history_title').offset().top<$('.img_down').offset().top){
-              $('html,body').animate({scrollTop: $('.history_title').offset().top},500);
+          else {
+            if($('.history_5').offset().top<$('.img_down').offset().top){
+              $('html,body').animate({scrollTop: $('.history_5').offset().top},500);
             }
             else {
-              if($('.p_where').offset().top<$('.img_down').offset().top){
-              $('html,body').animate({scrollTop: $('.p_where').offset().top},500);
+              if($('.history_4').offset().top<$('.img_down').offset().top){
+                $('html,body').animate({scrollTop: $('.history_4').offset().top},500);
               }
-              else{
-                $('html,body').animate({scrollTop: $('.p_time').offset().top},500);
+              else {
+                if($('.history_3').offset().top<$('.img_down').offset().top){
+                  $('html,body').animate({scrollTop: $('.history_3').offset().top},500);
+                }
+                else{
+                  if($('.history_2').offset().top<$('.img_down').offset().top){
+                    $('html,body').animate({scrollTop: $('.history_2').offset().top},500);
+                  }
+                  else{
+                    if ($('.history_1').offset().top<$('.img_down').offset().top) {
+                      $('html,body').animate({scrollTop: $('.history_1').offset().top},500);
+                    }
+                    else {
+                      if($('.history_title').offset().top<$('.img_down').offset().top){
+                        $('html,body').animate({scrollTop: $('.history_title').offset().top},500);
+                      }
+                      else {
+                        if($('.p_where').offset().top<$('.img_down').offset().top){
+                        $('html,body').animate({scrollTop: $('.p_where').offset().top},500);
+                        }
+                        else{
+                          $('html,body').animate({scrollTop: $('.p_time').offset().top},500);
+                        }
+                      }
+                    }
+                  }
+                }
               }
             }
           }
@@ -32,5 +60,10 @@ $(document).ready(function(){
       }
     }
   });
+});
+$(document).ready(function(){
+  if ($('.footer').offset().top<$('.img_down').offset().top) {
+    $(".img_down").addClass("display");
+  }
 });
   /*$(selector).animate(styles,speed,easing,callback)*/
