ccf49be9b2efb09abb0970edc8fe2adc000cf28e
unknown <>
2017-01-08 22:10:28 +0800

    first

--- js/123.js
+++ js/123.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
-  $('.man').addClass('.block');
-  if($('.man').offset().top<$('.balloon').offset().top){
-    $('.man').attr('display',block);
-  }
+  var img_height = $('#box').height();
+  var img_width = $('#box').width();
+  var img_x = $('.box').offset().left;
+  var img_y = $('.box').offset().top;
+  $('.img').click(function(){
+    $('.box').height( img_height+10);
+    $('.box').width(img_width+10);
+    $('#box').addClass('block');
+    $('#box').removeClass('display');
+    $("#box").offset(function(n,c){
+    	  newPos=new Object();
+        newPos.left=c.left+img_x-5;
+        newPos.top=c.top+5;
+        return newPos;
+    });
+  });
 });
--- js/吉卜力.js
+++ js/吉卜力.js
@@ -19,4 +19,122 @@ $(document).ready(function(){
     $("#where_up_img").removeClass("block");
     $("#where_down_img").removeClass("display");
   });
+  $(".img_down").click(function(){
+    if($('.history_21').offset().top<$('.img_down').offset().top){
+      $('html,body').animate({scrollTop: $('.history_21').offset().top},500);
+    }
+    else {
+      if($('.history_20').offset().top<$('.img_down').offset().top){
+        $('html,body').animate({scrollTop: $('.history_20').offset().top},500);
+      }
+      else {
+        if($('.history_19').offset().top<$('.img_down').offset().top){
+          $('html,body').animate({scrollTop: $('.history_19').offset().top},500);
+        }
+        else {
+          if($('.history_18').offset().top<$('.img_down').offset().top){
+            $('html,body').animate({scrollTop: $('.history_18').offset().top},500);
+          }
+          else {
+            if($('.history_17').offset().top<$('.img_down').offset().top){
+              $('html,body').animate({scrollTop: $('.history_17').offset().top},500);
+            }
+            else {
+              if($('.history_16').offset().top<$('.img_down').offset().top){
+                $('html,body').animate({scrollTop: $('.history_16').offset().top},500);
+              }
+              else {
+                if($('.history_15').offset().top<$('.img_down').offset().top){
+                  $('html,body').animate({scrollTop: $('.history_15').offset().top},500);
+                }
+                else {
+                  if($('.history_14').offset().top<$('.img_down').offset().top){
+                    $('html,body').animate({scrollTop: $('.history_14').offset().top},500);
+                  }
+                  else {
+                    if($('.history_13').offset().top<$('.img_down').offset().top){
+                      $('html,body').animate({scrollTop: $('.history_13').offset().top},500);
+                    }
+                    else {
+                      if($('.history_12').offset().top<$('.img_down').offset().top){
+                        $('html,body').animate({scrollTop: $('.history_12').offset().top},500);
+                      }
+                      else {
+                        if($('.history_11').offset().top<$('.img_down').offset().top){
+                          $('html,body').animate({scrollTop: $('.history_11').offset().top},500);
+                        }
+                        else {
+                          if($('.history_10').offset().top<$('.img_down').offset().top){
+                            $('html,body').animate({scrollTop: $('.history_10').offset().top},500);
+                          }
+                          else {
+                            if($('.history_9').offset().top<$('.img_down').offset().top){
+                              $('html,body').animate({scrollTop: $('.history_9').offset().top},500);
+                            }
+                            else {
+                              if($('.history_8').offset().top<$('.img_down').offset().top){
+                                $('html,body').animate({scrollTop: $('.history_8').offset().top},500);
+                              }
+                              else {
+                                if($('.history_7').offset().top<$('.img_down').offset().top){
+                                  $('html,body').animate({scrollTop: $('.history_7').offset().top},500);
+                                }
+                                else {
+                                  if($('.history_6').offset().top<$('.img_down').offset().top){
+                                    $('html,body').animate({scrollTop: $('.history_6').offset().top},500);
+                                  }
+                                  else {
+                                    if($('.history_5').offset().top<$('.img_down').offset().top){
+                                      $('html,body').animate({scrollTop: $('.history_5').offset().top},500);
+                                    }
+                                    else {
+                                      if($('.history_4').offset().top<$('.img_down').offset().top){
+                                        $('html,body').animate({scrollTop: $('.history_4').offset().top},500);
+                                      }
+                                      else {
+                                        if($('.history_3').offset().top<$('.img_down').offset().top){
+                                          $('html,body').animate({scrollTop: $('.history_3').offset().top},500);
+                                        }
+                                        else{
+                                          if($('.history_2').offset().top<$('.img_down').offset().top){
+                                            $('html,body').animate({scrollTop: $('.history_2').offset().top},500);
+                                          }
+                                          else{
+                                            if ($('.history_1').offset().top<$('.img_down').offset().top) {
+                                              $('html,body').animate({scrollTop: $('.history_1').offset().top},500);
+                                            }
+                                            else {
+                                              if($('.history_title').offset().top<$('.img_down').offset().top){
+                                                $('html,body').animate({scrollTop: $('.history_title').offset().top},500);
+                                              }
+                                              else {
+                                                if($('.img_where').offset().top<$('.img_down').offset().top){
+                                                $('html,body').animate({scrollTop: $('.img_where').offset().top},500);
+                                                }
+                                                else{
+                                                  $('html,body').animate({scrollTop: $('.img_time').offset().top},500);
+                                                }
+                                              }
+                                            }
+                                          }
+                                        }
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  });
 });
